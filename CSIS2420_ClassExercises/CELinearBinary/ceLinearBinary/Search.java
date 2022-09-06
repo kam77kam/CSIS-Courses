5cb188d5e4d1339a8466a2f460d72d256de621ca
kam77kam <Kamdonator@hotmail.com>
2022-09-05 22:44:19 -0600

    Completed Search.java Class. jUnit SearchTest.java Completed.

--- CSIS2420_ClassExercises/CELinearBinary/ceLinearBinary/Search.java
+++ CSIS2420_ClassExercises/CELinearBinary/ceLinearBinary/Search.java
@@ -1,6 +1,11 @@
+/*Author: Kamdon Bird
+ *Date: 9/5/2022
+ ClassExercise: CELinearBinary
+ */
+
 package ceLinearBinary;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,46 +13,20 @@ public class Search {
 	
 	public static int linear(int[] numbers, int key) {
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == key)
-                return i;
+            if (numbers[i] == key) {
+            	return i;
+            }
         }
         return -1;
 	}
 	//Binary Search
 	public static int binary(int[] numbers, int key) {
-//		if (numbers.length >= 1) {
-//			int mid = (numbers.length - 1) / 2;
-//			if (numbers[mid] == key) {
-//				return mid;
-//			}
-//			if (numbers[mid] > key) {
-//				mid = (numbers.length - mid)/2;
-//			}
-//			return -1;
-//		}
-		//Initialize numbers to be in a ArrayList to be sorted
-		List<Integer> numArray = new List<Integer>();
+//		Initialize numbers to be in a ArrayList to be sorted
+		List<Integer> numArray = new LinkedList<Integer>();
 		for (int value : numbers) {
 			numArray.add(value);
-		}
-		Collections.sort(numArray);
-		//Logic for Binary Search
-        int low = 0;
-        int high = numArray.size()-1;
-		while(low <= high) {
-			int mid = (low + high)/2;
-			if (numArray[mid] == ) {
-				return mid;
 			}
-		}
-		return -1;
+		Collections.sort(numArray);
+		return Collections.binarySearch(numArray, key);
 	}
-	public static void main(String[] args) {
-		int numbers[] = {10,15,20,25,30,35,40,45,50,55,60,65,70,75,5};
-		List<Integer> numArray = new ArrayList<Integer>();
-		for (int value : numbers) {
-			numArray.add(value);
-		}
-		System.out.print(numArray);
-	}
-}
+}	
--- CSIS2420_ClassExercises/jUnitTests/ceLinearBinary/SearchTest.java
+++ CSIS2420_ClassExercises/jUnitTests/ceLinearBinary/SearchTest.java
@@ -18,21 +18,21 @@ class SearchTest {
 		//expected stores the index of the list divided by 2
 		int expectedIndex = list.length/2;
 		int result = ceLinearBinary.Search.linear(list, list[expectedIndex]);
-		assertEquals(expectedIndex,result, "Middle element found: ");
+		assertEquals(expectedIndex,result, "(Linear) Middle element found: ");
 	}
 	@Test
 	void linear_lastElement_returnLastIndex() {
 		int list[] = {5,10,15,20,25,30,35,40,45,50,55,60,65,70,75};
 		int expectedIndex = list.length-1;
 		int result = ceLinearBinary.Search.linear(list, list[expectedIndex]);
-		assertEquals(expectedIndex,result, "Returned last element: ");
+		assertEquals(expectedIndex,result, "(Linear) Returned last element: ");
 	}
 	@Test
 	void linear_elementNotInArray_returnMinusOne() {
 		int list[] = {5,10,15,20,25,30,35,40,45,50,55,60,65,70,75};
 		int key = 1;
 		int result = ceLinearBinary.Search.linear(list, key);
-		assertNotEquals(key, result,"Element Not Found ");
+		assertNotEquals(key, result,"(Linear) Element Not Found ");
 	}
 	@Test
 	void linear_emptyArray_returnMinusOne() {
@@ -40,27 +40,44 @@ class SearchTest {
 		int key = 1;
 		int result = ceLinearBinary.Search.linear(list, key);
 		assertNotEquals(key, result,"Array Not Found ");
-		
 	}
-//	@Test
-//	void binary_firstElement_returnIndex0() {
-//		fail("Not yet implemented");
-//	}
-//	@Test
-//	void binary_middleElement_returnIndexOfElement() {
-//		fail("Not yet implemented");
-//	}
-//	@Test
-//	void binary_lastElement_returnLastIndex() {
-//		fail("Not yet implemented");
-//	}
-//	@Test
-//	void binary_elementNoInArray_returnminusOne() {
-//		fail("Not yet implemented");
-//	}
-//	@Test
-//	void binary_emptyArray_returnMinusOne() {
-//		fail("Not yet implemented");
-//	}
+	
+	//Binary jUnit Tests
+	@Test
+	void binary_willSort_returnIndex0() {
+		int list[] = {10,5};
+		int key = list[list.length-1];
+		int result = ceLinearBinary.Search.binary(list, key);
+		//Comparing index 0 to make sure the sort array properly changed
+		assertEquals(0, result, "Index moved properly ");
+	}
+	@Test
+	void binary_middleElement_returnIndexOfElement() {
+		int list[]= {5,10,15,20,25,30,35,40,45,50,55,60,65,70,75};
+		int key = list[list.length/2+1];
+		int result = ceLinearBinary.Search.binary(list, key);
+		assertEquals(8,result, "(Binary) Found the middle");
+	}
+	@Test
+	void binary_lastElement_returnLastIndex() {
+		int list[] = {5,10,15,20,25,30,35,40,45,50,55,60,65,70,75};
+		int expectedIndex = list.length-1;
+		int result = ceLinearBinary.Search.binary(list, list[expectedIndex]);
+		assertEquals(expectedIndex,result, "(Binary) Returned last element: ");
+	}
+	@Test
+	void binary_elementNoInArray_returnminusOne() {
+		int list[]= {5,10,15,20,25,30,35,40,45,50,55,60,65,70,75};
+		int key = 0;
+		int result = ceLinearBinary.Search.binary(list, key);
+		assertEquals(-1,result, "(Binary) Could not find the numbers in the List");
+	}
+	@Test
+	void binary_emptyArray_returnMinusOne() {
+		int list[] = {};
+		int key = 1;
+		int result = ceLinearBinary.Search.binary(list, key);
+		assertNotEquals(key, result,"(Binary) Array Not Found ");
+	}
 
 }
