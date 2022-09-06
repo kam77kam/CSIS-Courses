ffdff105ff250ba4f3cc7738c599e2a327e6a50c
Kamdon <kamdonator@hotmail.com>
2022-09-05 20:02:27 -0600

    More Modifications on Search.java

--- CSIS2420_ClassExercises/CELinearBinary/ceLinearBinary/Search.java
+++ CSIS2420_ClassExercises/CELinearBinary/ceLinearBinary/Search.java
@@ -1,8 +1,8 @@
 package ceLinearBinary;
 
-//import java.util.ArrayList;
-//import java.util.Collections;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Search {
 	
@@ -21,26 +21,33 @@ public class Search {
 //				return mid;
 //			}
 //			if (numbers[mid] > key) {
-//				
 //				mid = (numbers.length - mid)/2;
-//				
 //			}
-			return -1;
+//			return -1;
+//		}
+		//Initialize numbers to be in a ArrayList to be sorted
+		List<Integer> numArray = new List<Integer>();
+		for (int value : numbers) {
+			numArray.add(value);
 		}
-//		//Initialize numbers to be in a ArrayList to be sorted
-//		List<Integer> numArray = new ArrayList<Integer>();
-//		for (int value : numbers) {
-//			numArray.add(value);
-//		}
-//		Collections.sort(numArray);
-//		//Logic for Binary Search
-//		if(numArray.size() >= 1) {
-//			int mid = (numArray.size()/2);
-//			if (numArray[mid] == ) {
-//				return mid;
-//			}
-//			if ()
-//		}
-//		return -1;
-//	}
+		Collections.sort(numArray);
+		//Logic for Binary Search
+        int low = 0;
+        int high = numArray.size()-1;
+		while(low <= high) {
+			int mid = (low + high)/2;
+			if (numArray[mid] == ) {
+				return mid;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		int numbers[] = {10,15,20,25,30,35,40,45,50,55,60,65,70,75,5};
+		List<Integer> numArray = new ArrayList<Integer>();
+		for (int value : numbers) {
+			numArray.add(value);
+		}
+		System.out.print(numArray);
+	}
 }
