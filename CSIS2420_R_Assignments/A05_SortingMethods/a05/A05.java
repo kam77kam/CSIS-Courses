08cd551212598d728c13bd8e48392a2f77f0bb44
Kam77Kam <kamdonator@hotmail.com>
2023-10-07 21:28:30 -0600

    A05 Assignment Completed

--- CSIS2420_R_Assignments/A05_SortingMethods/a05/A05.java
+++ CSIS2420_R_Assignments/A05_SortingMethods/a05/A05.java
@@ -1,91 +1,223 @@
 package a05;
 
 import java.util.Arrays;
+import java.util.Random;
 
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.Stopwatch;
-
-
+/**
+ * @author Kamdon Bird, Haiyang Liu
+ */
 public class A05 {
 
- public static void main(String[] args) {
-     int N = 10000;
-     Double[] data = new Double[N];
+	/**
+	 * Generates a random array of integers with the specified size and number of
+	 * digits.
+	 *
+	 * @param arraySize      The size of the array to generate.
+	 * @param numberOfDigits The number of digits for each element in the array.
+	 * @return A random integer array.
+	 * @throws IllegalArgumentException if numberOfDigits is not between 1 and 10.
+	 */
+	private static Integer[] getRandomNumberArray(int arraySize, int numberOfDigits) {
+		if (numberOfDigits > 10 || numberOfDigits < 1) {
+			throw new IllegalArgumentException("The number of digits needs to be a value between 1 and 10.");
+		}
 
-     for (int i = 0; i < N; i++) {
-         data[i] = StdRandom.uniform(); // Generate random double values
-     }
+		Integer[] arr = new Integer[arraySize];
+		Random r = new Random();
 
-     System.out.println("Random data timings:");
-     displayTimings(data.clone());
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = r.nextInt(10) + 1;
+		}
 
-     // Sorted data
-     Arrays.sort(data);
-     System.out.println("\nSorted data timings:");
-     displayTimings(data.clone());
+		return arr;
+	}
 
-     // Reverse sorted data
-     Double[] reversed = new Double[N];
-     for (int i = 0; i < N; i++) {
-         reversed[i] = data[N - 1 - i];
-     }
-     System.out.println("\nReverse sorted data timings:");
-     displayTimings(reversed);
+	/**
+	 * Generates a sorted array of integers with the specified size and number of
+	 * digits.
+	 *
+	 * @param arraySize      The size of the array to generate.
+	 * @param numberOfDigits The number of digits for each element in the array.
+	 * @return A sorted integer array.
+	 */
+	private static Integer[] getSortedArray(int arraySize, int numberOfDigits) {
+		Integer[] arr = new Integer[arraySize];
+		Random r = new Random();
 
-     // Nearly sorted data (5% shuffled)
-     for (int i = 0; i < N * 0.05; i++) {
-         int idx1 = StdRandom.uniform(N);
-         int idx2 = StdRandom.uniform(N);
-         double temp = data[idx1];
-         data[idx1] = data[idx2];
-         data[idx2] = temp;
-     }
-     System.out.println("\nNearly sorted data timings (5% shuffled):");
-     displayTimings(data.clone());
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = r.nextInt();
+		}
 
-     // Duplicate data (only 10 unique values)
-     for (int i = 0; i < N; i++) {
-         data[i] = (double) StdRandom.uniform(10);
-     }
-     System.out.println("\nDuplicate data timings (only 10 unique values):");
-     displayTimings(data.clone());
- }
+		Arrays.sort(arr);
+		return arr;
+	}
 
- private static void displayTimings(Double[] data) {
-     double time1 = timeSortMethod(data.clone(), "sort1");
-     double time2 = timeSortMethod(data.clone(), "sort2");
-     double time3 = timeSortMethod(data.clone(), "sort3");
-     double time4 = timeSortMethod(data.clone(), "sort4");
+	/**
+	 * Generates a reversed sorted array of integers with the specified size and
+	 * number of digits.
+	 *
+	 * @param arraySize      The size of the array to generate.
+	 * @param numberOfDigits The number of digits for each element in the array.
+	 * @return A reversed sorted integer array.
+	 */
+	private static Integer[] getReversedArray(int arraySize, int numberOfDigits) {
+		Integer[] arr = new Integer[arraySize];
+		Random r = new Random();
 
-     System.out.println("Time for sort1: " + time1);
-     System.out.println("Time for sort2: " + time2);
-     System.out.println("Time for sort3: " + time3);
-     System.out.println("Time for sort4: " + time4);
- }
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = r.nextInt();
+		}
 
+		Arrays.sort(arr);
 
+		for (int i = 0; i < arr.length / 2; i++) {
+			int j = arr.length - 1 - i;
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
 
+		return arr;
+	}
 
-    private static double timeSortMethod(Double[] data, String methodName) {
-        Stopwatch timer = new Stopwatch();
+	// =========================================TEST CLIENT======================================================
+	public static void main(String[] args) {
+		//Random number array to test all sorts of Mystery.
+		System.out.println("Random Number Array: ");
+		System.out.println("        n | sort1      | sort2      | sort3      | sort4      |");
+		System.out.println("----------|------------|------------|------------|------------|");
 
-        switch (methodName) {
-            case "sort1":
-                Mystery.sort1(data);
-                break;
-            case "sort2":
-                Mystery.sort2(data);
-                break;
-            case "sort3":
-                Mystery.sort3(data);
-                break;
-            case "sort4":
-                Mystery.sort4(data);
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid method name: " + methodName);
-        }
+		int n = 1000;
 
-        return timer.elapsedTime();
-    }
+		for (int i = 0; i < 7; i++) {
+			Integer[] sort1 = getRandomNumberArray(n, 7);
+			Integer[] sort2 = getRandomNumberArray(n, 7);
+			Integer[] sort3 = getRandomNumberArray(n, 7);
+			Integer[] sort4 = getRandomNumberArray(n, 7);
+
+			long start = System.nanoTime();
+			Mystery.sort1(sort1);
+			double sort1Time = (System.nanoTime() - start) / 1e9;
+
+			start = System.nanoTime();
+			Mystery.sort2(sort2);
+			double sort2Time = (System.nanoTime() - start) / 1e9;
+
+			start = System.nanoTime();
+			Mystery.sort3(sort3);
+			double sort3Time = (System.nanoTime() - start) / 1e9;
+
+			start = System.nanoTime();
+			Mystery.sort4(sort4);
+			double sort4Time = (System.nanoTime() - start) / 1e9;
+
+			System.out.printf("%9d | %8.4fs  | %8.4fs  | %8.4fs  | %8.4fs  | \n", n, sort1Time, sort2Time, sort3Time,
+					sort4Time);
+			n *= 2;
+		}
+		System.out.println();
+		//Sorted number array to test all sorts of Mystery.
+		System.out.println("Sorted Number Array: ");
+		System.out.println("        n | sort1      | sort2      | sort3      | sort4      |");
+		System.out.println("----------|------------|------------|------------|------------|");
+
+		n = 1000;
+
+		for (int i = 0; i < 7; i++) {
+			Integer[] sort1 = getSortedArray(n, 7);
+			Integer[] sort2 = getSortedArray(n, 7);
+			Integer[] sort3 = getSortedArray(n, 7);
+			Integer[] sort4 = getSortedArray(n, 7);
+
+			long start = System.nanoTime();
+			Mystery.sort1(sort1);
+			double sort1Time = (System.nanoTime() - start) / 1e9;
+
+			start = System.nanoTime();
+			Mystery.sort2(sort2);
+			double sort2Time = (System.nanoTime() - start) / 1e9;
+
+			start = System.nanoTime();
+			Mystery.sort3(sort3);
+			double sort3Time = (System.nanoTime() - start) / 1e9;
+
+			start = System.nanoTime();
+			Mystery.sort4(sort4);
+			double sort4Time = (System.nanoTime() - start) / 1e9;
+
+			System.out.printf("%9d | %8.4fs  | %8.4fs  | %8.4fs  | %8.4fs  | \n", n, sort1Time, sort2Time, sort3Time,
+					sort4Time);
+			n *= 2;
+		}
+		System.out.println();
+		//Reversed number array to test all sorts of Mystery.
+		System.out.println("Reversed Sorted Number Array: ");
+		System.out.println("        n | sort1      | sort2      | sort3      | sort4      |");
+		System.out.println("----------|------------|------------|------------|------------|");
+
+		n = 1000;
+
+		for (int i = 0; i < 7; i++) {
+			Integer[] sort1 = getReversedArray(n, 7);
+			Integer[] sort2 = getReversedArray(n, 7);
+			Integer[] sort3 = getReversedArray(n, 7);
+			Integer[] sort4 = getReversedArray(n, 7);
+
+			long start = System.nanoTime();
+			Mystery.sort1(sort1);
+			double sort1Time = (System.nanoTime() - start) / 1e9;
+
+			start = System.nanoTime();
+			Mystery.sort2(sort2);
+			double sort2Time = (System.nanoTime() - start) / 1e9;
+
+			start = System.nanoTime();
+			Mystery.sort3(sort3);
+			double sort3Time = (System.nanoTime() - start) / 1e9;
+
+			start = System.nanoTime();
+			Mystery.sort4(sort4);
+			double sort4Time = (System.nanoTime() - start) / 1e9;
+
+			System.out.printf("%9d | %8.4fs  | %8.4fs  | %8.4fs  | %8.4fs  | \n", n, sort1Time, sort2Time, sort3Time,
+					sort4Time);
+			n *= 2;
+		}
+		System.out.println();
+		//Large number array to test all sorts 2 and 3 of Mystery.
+		System.out.println("Large Array: ");
+		System.out.println("        n | sort2      | sort3      |");
+		System.out.println("----------|------------|------------|");
+
+		int n3 = 1_000_000;
+
+		for (int j = 0; j < 7; j++) {
+
+			Integer[] arr1 = new Integer[n3];
+			Integer[] arr2 = new Integer[n3];
+			Random r = new Random();
+
+			for (int i = 0; i < arr1.length; i++) {
+				arr1[i] = r.nextInt();
+			}
+
+			for (int i = 0; i < arr2.length; i++) {
+				arr2[i] = r.nextInt();
+			}
+
+			long start = System.nanoTime();
+			Mystery.sort2(arr1);
+			double sort2Time = (System.nanoTime() - start) / 1e9;
+
+			start = System.nanoTime();
+			Mystery.sort3(arr2);
+			double sort3Time = (System.nanoTime() - start) / 1e9;
+
+			System.out.printf("%9d | %8.4fs  | %8.4fs  | \n", n3, sort2Time, sort3Time);
+			
+			n3 *= 2;
+		}
+		System.out.println();
+	}
+
 }
