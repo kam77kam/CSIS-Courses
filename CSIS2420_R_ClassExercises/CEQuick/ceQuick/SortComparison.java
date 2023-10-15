ea108c52aa7e54c5929009de7fad6b3790dc2f59
Kam77Kam <kamdonator@hotmail.com>
2023-10-15 16:07:44 -0600

    Added Java Doc, Completed CEQuick and CEStable

--- CSIS2420_R_ClassExercises/CEQuick/ceQuick/SortComparison.java
+++ CSIS2420_R_ClassExercises/CEQuick/ceQuick/SortComparison.java
@@ -1,39 +1,90 @@
-/**
- * 
- */
 package ceQuick;
 
-import java.util.Arrays;
+import edu.princeton.cs.algs4.Selection;
+import edu.princeton.cs.algs4.Quick;
 import edu.princeton.cs.algs4.StdRandom;
 
 /**
+ * A class that provides methods for generating arrays of random integers with a
+ * specified number of digits and comparing the performance of selection sort
+ * and quicksort algorithms on these arrays.
+ *
+ * This class contains methods for generating random integer arrays and
+ * measuring the execution time of the selection sort and quicksort algorithms
+ * on these arrays. It includes a test client in the `main` method that performs
+ * performance comparisons and prints the results to the standard output.
+ * 
  * @author Kamdon Bird
  */
 public class SortComparison {
 
-	private static int[] getRandomNumberArray(int arraySize, int numberOfDigits) {
-		if(numberOfDigits < 0 || 10 < numberOfDigits) 
-			throw new IllegalArgumentException();
-		int[] array = new int[arraySize];
+	/**
+	 * Generates an array of random integers with a specified number of digits.
+	 *
+	 * This method generates an array of random integers, where each integer has a
+	 * specified number of digits. The range of generated integers is determined by
+	 * the number of digits, which should be between 1 and 10. The array size is
+	 * specified by the 'arraySize' parameter.
+	 *
+	 * @param arraySize      The size of the array to be generated.
+	 * @param numberOfDigits The number of digits for each generated integer
+	 * @return An array of random integers with the specified number of digits.
+	 * @throws IllegalArgumentException If the 'numberOfDigits' parameter is not
+	 *                                  within the range of 1 to 10.
+	 */
+	private static Integer[] getRandomNumberArray(int arraySize, int numberOfDigits) {
+		if (numberOfDigits < 0 || 10 < numberOfDigits)
+			throw new IllegalArgumentException("The number of digits needs to be a value between 1 and 10");
+
+		Integer[] array = new Integer[arraySize];
 		int lowerBound = 1;
 		int upperBound = 10;
-		for(int i = 1; i < numberOfDigits; i++) {
-			lowerBound = lowerBound * 10;
-			upperBound = upperBound * 10;
+
+		for (int i = 1; i < numberOfDigits; i++) {
+			lowerBound *= 10;
+			upperBound *= 10;
 		}
+
 		for (int i = 0; i < arraySize; i++) {
 			array[i] = StdRandom.uniform(lowerBound, upperBound);
 		}
+
 		return array;
 	}
 
-//=================TEST CLIENT=================
+	/**
+	 * The main method that performs performance comparisons of selection sort and
+	 * quicksort on arrays of random integers.
+	 * This method generates random integer arrays of increasing sizes and measures
+	 * the execution time of selection sort and quicksort on these arrays. The
+	 * results are printed to the standard output.
+	 *
+	 * @param args The command-line arguments (not used in this program).
+	 */
 	public static void main(String[] args) {
-		int[] array = getRandomNumberArray(10, 7);
-//		for(int a: array) {
-//			System.out.println(a);
-//		}
-		System.out.println(Arrays.toString(array));
-	}
+		System.out.println("      n | Selection | Quick     |");
+		System.out.println("--------------------------------|");
+		int arraySize = 1000;
+		long startTime, endTime;
 
+		while (arraySize <= 512_000) {
+			Comparable<Integer>[] arr = getRandomNumberArray(arraySize, 7);
+			Comparable<Integer>[] arrCopy = arr;
+
+			startTime = System.nanoTime();
+			Selection.sort(arr);
+			endTime = System.nanoTime();
+			double selectionSortTime = (endTime - startTime) / 1e9;
+
+			startTime = System.nanoTime();
+			Quick.sort(arrCopy);
+			endTime = System.nanoTime();
+			double quickSortTime = (endTime - startTime) / 1e9;
+
+			System.out.printf("%7d | %8.4fs | %.4fs", arraySize, selectionSortTime, quickSortTime);
+			System.out.println("   |");
+
+			arraySize *= 2;
+		}
+	}
 }
--- CSIS2420_R_ClassExercises/CEStable/ceStable/StableApp.java
+++ CSIS2420_R_ClassExercises/CEStable/ceStable/StableApp.java
@@ -13,16 +13,31 @@ import edu.princeton.cs.algs4.Insertion;
 /**
  * Demonstrates how stable sorting algorithms work.
  * 
+ * This class provides a demonstration of stable sorting algorithms using song
+ * data read from a CSV file. It showcases both non-stable and stable sorting
+ * algorithms and their effects on the order of the songs in the output. The
+ * class also includes a method to read song data from a CSV file and methods to
+ * print and manipulate the song data.
+ * 
  * @author Kamdon Bird
  */
 
 public class StableApp {
-	
+
 	/**
-	 * Reads in the songs from the csv file and returns them as an array of songs.
-	 * 
-	 * @param fileName
-	 * @return
+	 * Reads song data from a CSV file and returns an array of Song objects.
+	 *
+	 * This method reads song data from a CSV (Comma-Separated Values) file, where
+	 * each line represents a song with a name and artist. It creates Song objects
+	 * for each song and stores them in an array. The CSV file should have a header
+	 * row, and the data for each song should be in the format "name,artist". If the
+	 * file cannot be read or an error occurs during the process, an error message
+	 * is printed to the standard output, and any exception is printed to the
+	 * standard error stream.
+	 *
+	 * @param fileName The name of the CSV file containing song data.
+	 * @return An array of Song objects representing the songs from the CSV file.
+	 * @throws IOException If an I/O error occurs while reading the file.
 	 */
 	private static Song[] getSongs(String fileName) {
 		List<Song> list = new ArrayList<>();
@@ -42,6 +57,14 @@ public class StableApp {
 		return list.toArray(new Song[list.size()]);
 	}
 
+	/**
+	 * The main method that demonstrates stable sorting algorithms.
+	 *
+	 * This method reads song data from a CSV file, performs both non-stable and
+	 * stable sorting on the data, and displays the results in the console.
+	 *
+	 * @param args The command-line arguments (not used in this program).
+	 */
 	public static void main(String[] args) {
 		/*
 		 * Printing CSV file
@@ -53,8 +76,8 @@ public class StableApp {
 			System.out.println(s);
 		}
 		/*
-		 * Printing a Non Stable Sorting (Selection.sort) and then
-		 * shuffling the songs, review song 8 (line 8) in the output
+		 * Printing a Non Stable Sorting (Selection.sort) and then shuffling the songs,
+		 * review song 8 (line 8) in the output
 		 */
 		System.out.println("*** demo NOT STABLE sorting ***");
 		System.out.println();
@@ -73,8 +96,7 @@ public class StableApp {
 		System.out.println();
 		Knuth.shuffle(songs);
 		/*
-		 * Printing a Stable Sorting (Arrays.sort) and then
-		 * shuffling the songs
+		 * Printing a Stable Sorting (Arrays.sort) and then shuffling the songs
 		 */
 		System.out.println("*** demo STABLE sorting ***");
 		System.out.println();
@@ -93,8 +115,7 @@ public class StableApp {
 		for (Song s : songs) {
 			System.out.println(s);
 		}
-		
+
 	}
 
-	
 }
