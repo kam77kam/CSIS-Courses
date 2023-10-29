0e091f6a5c8dd66d51c58b0656431afe970f8794
Kam77Kam <kamdonator@hotmail.com>
2023-10-28 20:29:12 -0600

    Finished A06 - Symbol Table Assignment

--- CSIS2420_R_Assignments/A06_SymbolTable/a06/Movie.java
+++ CSIS2420_R_Assignments/A06_SymbolTable/a06/Movie.java
@@ -1,71 +1,31 @@
 package a06;
 
 /**
+ * Represents a movie with attributes like rank, title, director, runtime (in minutes), and rating.
  * @author Kamdon Bird, Haiyang Liu
  */
 public class Movie {
-	private Integer rank;
+	private int rank;
 	private String title;
 	private String director;
 	private int minutes;
 	private double rating;
-//	
-//	public Movie(String rank, String title, String director, String minutes, String rating) {
-//		this.rank = rank;
-//		this.title = title;
-//		this.director = director;
-//		this.minutes = minutes;
-//		this.rating = rating;
-//	}
-//	public int getRank() {
-//		return rank;
-//	}
-//	public void setRank(String rank) {
-//		if(Integer.getInteger(rank) < 0) {
-//			throw new NumberFormatException("rank needs to be positive and a number");
-//		}
-//		this.rank = Integer.getInteger(rank);
-//	}
-//	public String getTitle() {
-//		return title;
-//	}
-//	public void setTitle(String title) {
-//		this.title = title;
-//	}
-//	public String getDirector() {
-//		return director;
-//	}
-//	public void setDirector(String director) {
-//		this.director = director;
-//	}
-//	public int getMinutes() {
-//		return minutes;
-//	}
-//	public void setMinutes(String minutes) {
-//		if(Integer.getInteger(minutes) < 0) {
-//			throw new NumberFormatException("minutes needs to be positive and a number");
-//		}
-//		this.minutes = Integer.getInteger(minutes);
-//	}
-//	public double getRating() {
-//		return rating;
-//	}
-//	public void setRating(double rating) {
-//		if() {
-//			
-//		}
-//	}
-//	@Override
-//	public String toString() {
-//		return "Movie [rank=" + rank + ", title=" + title + ", director=" + director + ", minutes=" + minutes
-//				+ ", rating=" + rating + "]";
-//	}
-//	
 
+	/**
+	 * Default constructor, initializes a blank movie object.
+	 */
 	public Movie() {
 
 	}
 
+	/**
+	 * Parameterized constructor to initialize a movie with specific attributes.
+	 * @param rank     The rank or position of the movie.
+	 * @param title    The title of the movie.
+	 * @param director The director of the movie.
+	 * @param minutes  The runtime of the movie in minutes.
+	 * @param rating   The rating score of the movie.
+	 */
 	public Movie(int rank, String title, String director, int minutes, double rating) {
 		this.rank = rank;
 		this.title = title;
@@ -114,10 +74,14 @@ public class Movie {
 		this.rating = rating;
 	}
 
+	/**
+	 * Returns the movie's representation as a string.
+	 * @return A string representation of the movie.
+	 */
 	@Override
 	public String toString() {
-		return "Movie [rank=" + rank + ", title=" + title + ", director=" + director + ", minutes=" + minutes
-				+ ", rating=" + rating + "]";
+		return "Movie [Rank = " + rank + ", Title = " + title + ", Director=" + director + ", Minutes=" + minutes
+				+ ", Rating = " + rating + "]";
 	}
 
 }
--- CSIS2420_R_Assignments/A06_SymbolTable/a06/MovieApp.java
+++ CSIS2420_R_Assignments/A06_SymbolTable/a06/MovieApp.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package a06;
 
 import java.io.BufferedReader;
@@ -8,12 +5,27 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import edu.princeton.cs.algs4.RedBlackBST;
-
 /**
- * @author Kamdon Bird, Haiyang Liu
+ *Represents a movie application that performs various operations on a collection of movies stored in a RedBlackBST.
  */
 public class MovieApp {
-
+	
+	/**
+	 * Prints all the movies stored in the provided RedBlackBST.
+	 * @param bst The RedBlackBST containing movies.
+	 */
+	private static void printMovies(RedBlackBST<String, Movie> bst) {
+		// Print out all movies from the RedBlackBST
+		for (String rank : bst.keys()) {
+			Movie movie = bst.get(rank);
+			System.out.println(movie);
+		}
+	}
+	/**
+	 * Fetches an array of movies from a CSV file.
+	 * @param fileName The path to the CSV file.
+	 * @return An array of movies read from the file.
+	 */
 	private static Movie[] getMovies(String fileName) {
 		ArrayList<Movie> list = new ArrayList<>();
 
@@ -41,41 +53,135 @@ public class MovieApp {
 
 		return list.toArray(new Movie[list.size()]);
 	}
-
+	/**
+	 * A utility method to split a CSV line into its respective fields.
+	 * Note: This is a simple implementation and might not handle all CSV edge cases.
+	 * @param line The CSV line to be split.
+	 * @return An array containing individual fields.
+	 * */
 	private static String[] simpleSplitCsvLine(String line) {
 		line = line.replace("\"", ""); // remove all double quotes
 		return line.split(","); // split by commas
 	}
-
+	/**
+	 * Entry point of the application.
+	 * @param args command-line arguments (not used)
+	 */
 	public static void main(String[] args) {
 
-//		System.out.println("Movies from CSV file");//TODO
-//		System.out.println("-------------------");
-//		Movie[] movies = getMovies("A06_SymbolTable/a06/Resources/IMDB-Movie-Data.csv");
-//
-//		for (Movie s : movies) {
-//			System.out.println(s);
-//		}
-//		System.out.println();
-
-		/**
-		 * 
-		 * put get delete contains isEmpty size min or max floor or ceiling rank select
-		 * deleteMin or deleteMax keys
-		 * 
-		 */
-
 		Movie[] movies = getMovies("A06_SymbolTable/a06/Resources/IMDB-Movie-Data.csv");
-		// RedBlackBST for title
-		RedBlackBST<Integer, Movie> bst = new RedBlackBST<>();
-		Movie movie21 = new Movie(21, "New Movie Title 1", "New Movie Director 1", 120, 7.5);
-		Movie movie22 = new Movie(22, "New Movie Title 2", "New Movie Director 2", 130, 8.0);
-		for (Movie s : movies) {
-			bst.put(s.getRank(), s);
+
+		RedBlackBST<String, Movie> bst = new RedBlackBST<>();
+		RedBlackBST<Integer, Movie> bst2 = new RedBlackBST<>();
+
+		bst.put("New Movie Title 21", new Movie(21, "New Movie Title 1", "Director 21", 120, 8.5));
+		bst.put("New Movie Title 22", new Movie(22, "New Movie Title 2", "Director 22", 90, 8.9));
+		bst.put("New Movie Title 23", new Movie(23, "New Movie Title 3", "Director 23", 125, 8.2));
+		bst.put("New Movie Title 24", new Movie(24, "New Movie Title 4", "Director 24", 95, 8.4));
+
+		// Add movies from the CSV file to the RedBlackBST
+		for (Movie movie : movies) {
+			bst.put(movie.getTitle(), movie);
+			bst2.put(movie.getRank(), movie);
 		}
-		bst.put(movie21.getRank(), movie21);
-		System.out.println(bst.get(movie21.getRank()));
-//		System.out.println(bst.get(s.getRank())); //TODO
+		System.out.println("1. Put: ");
+		printMovies(bst);
+
+		System.out.println("-----------------------");
+		System.out.println("2. Get: ");
+
+		Movie m = bst.get("The Great Wall");
+		System.out.println(m);
+		System.out.println(bst.get("The Lost City of Z"));
+
+		System.out.println("-----------------------");
+		System.out.println("3. Delete: ");
+		System.out.println("Deleting 'Arrival'...");
+		bst.delete("Arrival");
+		printMovies(bst);
+
+		System.out.println("-----------------------");
+		System.out.println("4. Contains: ");
+		System.out.println("Looking for 'Sing' title... (expecting true)");
+		Boolean result = bst.contains("Sing");
+		System.out.println("Result: " + result);
+
+		System.out.println("Looking for 'The Toys' title... (expecting false)");
+		Boolean result1 = bst.contains("The Toys");
+		System.out.println("Result: " + result1);
+
+		System.out.println("-----------------------");
+		System.out.println("5. isEmpty: ");
+		System.out.println("Checking current Red-Black-BST... (expecting false)");
+		boolean r = bst.isEmpty();
+		System.out.println("Result: " + r);
+
+		RedBlackBST<String, Movie> bst1 = new RedBlackBST<>();
+		System.out.println("Creating a new Red-Black-BST... (expecting true)");
+		boolean r1 = bst1.isEmpty();
+		System.out.println("Result: " + r1);
+
+		System.out.println("-----------------------");
+		System.out.println("6. Size: ");
+		int size = bst.size();
+		System.out.println("Movies returned: " + size);
+
+		System.out.println("-----------------------");
+		System.out.println("7. Min or max: ");
+		String min = bst.min();
+		System.out.println("Min : " + min);
+		String max = bst.max();
+		System.out.println("Max : " + max);
+
+		System.out.println("-----------------------");
+		System.out.println("8. Floor or ceiling: ");
+		// floor pass
+		System.out.println("Searching for 'Suicide Squad' using floor...");
+		String floor = bst.floor("Suicide Squad");
+		System.out.println("Floor : " + floor);
+		System.out.println();
+		// floor failed to lower node
+		System.out.println("Searching for 'Suicide' using floor...");
+		String floor2 = bst.floor("Suicide");
+		System.out.println("Floor (expecting 'Split'): " + floor2);
+		System.out.println();
+		// ceiling pass
+		System.out.println("Searching for 'Passengers' using ceiling...");
+		String ceiling = bst.ceiling("Passengers");
+		System.out.println("Ceiling : " + ceiling);
+		System.out.println();
+		// ceiling failed to higher node
+		System.out.println("Searching for 'Pass' using ceiling...");
+		String ceiling2 = bst.ceiling("Pass");
+		System.out.println("Ceiling (expecting 'Passengers'): " + ceiling2);
+
+		System.out.println("-----------------------");
+		System.out.println("9. Rank: ");
+		int rank = bst.rank("Mindhorn");
+		System.out.println("Checking The Title Mindhorn and its corresponding rank : " + rank);
+
+		System.out.println("-----------------------");
+		System.out.println("10. Select: ");
+		System.out.println("Selecting 3rd element in Red-Black-BST...");
+		String select = bst.select(2);
+		System.out.println("Returned : " + select);
+
+		System.out.println("-----------------------");
+		System.out.println("11. DeleteMax or DeleteMin : ");
+		System.out.println("Deleting movie 'Colossal'(min)..." );
+		System.out.println("Deleting movie 'The Secret Life of Pets'(max)...");
+		System.out.println("Movies after delete Max and Min");
+		bst.deleteMax();
+		bst.deleteMin();
+		printMovies(bst);
+
+		System.out.println("-----------------------");
+		System.out.println("12. Keys: ");
+		for (String title : bst.keys()) {
+			Movie movie = bst.get(title);
+			System.out.println(movie.getTitle());
+		}
+
 	}
 
 }
